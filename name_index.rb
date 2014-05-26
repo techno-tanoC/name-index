57b60b8814ba0889947eda3f5480a98d5b31117c
techno-tanoC <techno.tanoc@gmail.com>
2014-05-26 17:30:10 +0900

    answer

--- name_index.rb
+++ name_index.rb
@@ -1,5 +1,27 @@
 class NameIndex
+  @@COLS = ["ア-オ",
+           "カ-ゴ",
+           "サ-ゾ",
+           "タ-ド",
+           "ナ-ノ",
+           "ハ-ポ",
+           "マ-モ",
+           "ヤ-ヨ",
+           "ラ-ロ",
+           "ワ-ン"]
+
+  def self.match(name)
+    @@COLS.find {|col| name =~ /\A[#{col}]/ }.chr
+  end 
+
   def self.create_index(names)
-    []
+    names
+    .sort
+    .group_by(&method(:match))
+    .to_a
   end
 end
+
+inp = ['キシモト', 'イトウ', 'ババ', 'カネダ', 'ワダ', 'ハマダ']
+
+p NameIndex.create_index(inp)
--- spec/name_index_spec.rb
+++ spec/name_index_spec.rb
@@ -1,5 +1,20 @@
 require File.expand_path(File.dirname(__FILE__) + '/../name_index')
 
 describe NameIndex do
+  specify { expect(NameIndex.match("キシモト")).to eq "カ" }
+
+  specify { expect(NameIndex.match("イトウ")).to eq "ア" }
+
+  specify { expect(NameIndex.match("ババ")).to eq "ハ" }
+
+  specify { expect(NameIndex.match("カネダ")).to eq "カ" }
+
+  specify { expect(NameIndex.match("ワダ")).to eq "ワ" }
+
+  specify { expect(NameIndex.match("ハマダ")).to eq "ハ" }
+
   specify { expect(NameIndex.create_index([])).to eq [] }
+
+  specify { expect(NameIndex.create_index(['キシモト', 'イトウ', 'ババ', 'カネダ', 'ワダ', 'ハマダ']))
+            .to eq [ ['ア', ['イトウ']], ['カ', ['カネダ', 'キシモト']], ['ハ', ['ハマダ', 'ババ']], ['ワ', ['ワダ']]] }
 end
